aef14e9f52bbbbe09a0f40fdd76ee192c2235e5a
navnit.amcodr <navnit.amcodr@gmail.com>
2023-07-26 11:38:29 +0530

    Join requests link added in join request notification mail

--- src/views/emails/approval-notification.blade.php
+++ src/views/emails/approval-notification.blade.php
@@ -3,6 +3,10 @@
 <p>Request to Join {{ $data['organization_name'] }} has been received from {{$data['sender_name']}}</p>
  
 <p>Kindly take necessary action on same</p>
+
+@component('mail::button', ['url' => route('organization.request.list')])
+Join Requests
+@endcomponent
  
 Thanks,<br>
 {{$data['organization_name']}}<br>
