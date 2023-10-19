09f6ab86ef3d822bc2a1ce441f428346a214136c
navnit.amcodr <navnit.amcodr@gmail.com>
2023-10-19 16:03:31 +0530

    menu restricted for non organization user

--- src/views/partials/sidemenu-item.blade.php
+++ src/views/partials/sidemenu-item.blade.php
@@ -14,14 +14,16 @@
 		      <p>{{ __('orgmgmt::organization.header.my_org_list') }}</p>
 		    </a>
 		</li>	
-		<li class="nav-item">
-		    <a href="{{ route('organization.invite') }}" class="pl-4 nav-link">
-		    	<i class="nav-icon fa fa-user-plus"></i>
-		      	<p>
-		        	{{ __('orgmgmt::organization.header.invite_org') }}
-		    	</p>
-		    </a>
-		</li>
+		@if(count(auth()->user()->userOrganizations()->get()))
+			<li class="nav-item">
+			    <a href="{{ route('organization.invite') }}" class="pl-4 nav-link">
+			    	<i class="nav-icon fa fa-user-plus"></i>
+			      	<p>
+			        	{{ __('orgmgmt::organization.header.invite_org') }}
+			    	</p>
+			    </a>
+			</li>
+		@endif
 		<li class="nav-item">
 		    <a href="{{ route('organization.join.list') }}" class="pl-4 nav-link">		    	
 		    	<i class="nav-icon fa fa-university"></i>
@@ -30,22 +32,24 @@
 		    	</p>
 		    </a>
 		</li>
-		<li class="nav-item">
-		    <a href="{{ route('organization.request.list') }}" class="pl-4 nav-link">		    	
-		    	<i class="nav-icon fa fa-list"></i>
-		      	<p>
-		        	{{ __('orgmgmt::organization.header.join_requests') }}		        	
-		    	</p>
-		    </a>
-		</li>
-		<li class="nav-item">
-		    <a href="{{ route('organization.members') }}" class="pl-4 nav-link">
-		    	<i class="nav-icon fa fa-user-friends"></i>
-		      	<p>
-		        	{{ __('orgmgmt::organization.header.org_members') }}
-		    	</p>
-		    </a>
-		</li>		
+		@if(count(auth()->user()->userOrganizations()->get()))
+			<li class="nav-item">
+			    <a href="{{ route('organization.request.list') }}" class="pl-4 nav-link">		    	
+			    	<i class="nav-icon fa fa-list"></i>
+			      	<p>
+			        	{{ __('orgmgmt::organization.header.join_requests') }}		        	
+			    	</p>
+			    </a>
+			</li>
+			<li class="nav-item">
+			    <a href="{{ route('organization.members') }}" class="pl-4 nav-link">
+			    	<i class="nav-icon fa fa-user-friends"></i>
+			      	<p>
+			        	{{ __('orgmgmt::organization.header.org_members') }}
+			    	</p>
+			    </a>
+			</li>		
+		@endif
 		@else
 		<li class="nav-item">
 		    <a href="{{ route('organization.list') }}" class="pl-4 nav-link">
