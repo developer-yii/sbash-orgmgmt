ce33850497f9a613a3d265f93d1acca786a6c1b4
navnit.amcodr <navnit.amcodr@gmail.com>
2024-01-02 12:20:30 +0530

    left margin added in sidemenu sub item

--- src/views/partials/sidemenu-item.blade.php
+++ src/views/partials/sidemenu-item.blade.php
@@ -9,14 +9,14 @@
 	<ul class="nav nav-treeview">	  
 		@role('User')			
 		<li class="nav-item">
-		    <a href="{{ route('organization.mylist') }}" class="pl-4 nav-link">
+		    <a href="{{ route('organization.mylist') }}" class="ml-4 nav-link">
 		      <i class="fa fa-cogs nav-icon"></i>
 		      <p>{{ __('orgmgmt')['header']['my_org_list'] }}</p>
 		    </a>
 		</li>	
 		@if(count(auth()->user()->userOrganizations()->get()))
 			<li class="nav-item">
-			    <a href="{{ route('organization.invite') }}" class="pl-4 nav-link">
+			    <a href="{{ route('organization.invite') }}" class="ml-4 nav-link">
 			    	<i class="nav-icon fa fa-user-plus"></i>
 			      	<p>
 			        	{{ __('orgmgmt')['header']['invite_org'] }}
@@ -36,7 +36,7 @@
 		@endif --}}
 		@if(count(auth()->user()->userOrganizations()->get()))
 			<li class="nav-item">
-			    <a href="{{ route('organization.request.list') }}" class="pl-4 nav-link">		    	
+			    <a href="{{ route('organization.request.list') }}" class="ml-4 nav-link">		    	
 			    	<i class="nav-icon fa fa-list"></i>
 			      	<p>
 			        	{{ __('orgmgmt')['header']['join_requests'] }}		        	
@@ -44,7 +44,7 @@
 			    </a>
 			</li>
 			<li class="nav-item">
-			    <a href="{{ route('organization.members') }}" class="pl-4 nav-link">
+			    <a href="{{ route('organization.members') }}" class="ml-4 nav-link">
 			    	<i class="nav-icon fa fa-user-friends"></i>
 			      	<p>
 			        	{{ __('orgmgmt')['header']['org_members'] }}
@@ -54,7 +54,7 @@
 		@endif
 		@else
 		<li class="nav-item">
-		    <a href="{{ route('organization.list') }}" class="pl-4 nav-link">
+		    <a href="{{ route('organization.list') }}" class="ml-4 nav-link">
 		    	<i class="nav-icon fa fa-user-friends"></i>
 		      	<p>
 		        	{{ __('orgmgmt')['header']['org_list'] }}
